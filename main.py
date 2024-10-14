34b0153337a7a368295bb8531c87888ff50970ce
Chintu43219 <bandnabad@gmail.com>
2024-10-14 23:03:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ def install_requirements():
 install_requirements()
 
 # 🎛️ Telegram API token (replace with your actual token)
-TOKEN = '7424479555:AAGtNAbqXax_b3b--QnGYi5_XwBbQNw'
+TOKEN = '7245510353:AAGxra7164KMN5uNB74aKEqpgOfGLTzcVEc'
 bot = telebot.TeleBot(TOKEN, threaded=False)
 
 # 🛡️ List of authorized user IDs (replace with actual IDs)
